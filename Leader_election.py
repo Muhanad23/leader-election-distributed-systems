bb58083c9d26384c26a5f893ff6382395850c88f
Muhanad <muhanad.atef23@gmail.com>
2020-03-20 22:49:10 +0200

    Initialize All My Connection

--- Leader_election.py
+++ Leader_election.py
@@ -2,10 +2,28 @@ import zmq
 import socket
 import time
 
-def initializeAsLeader(numberOfClients,myID,basePort):
+# IParray: holding IP's for all machines in the system, myID: containing my ID (index), basePort: 
+def initializeMySockets(IParray,myID,basePort):
+    # Publisher for publishing I'm alive: topic 1 (if master) else ok msg: topic 2 (to reply for clients which ask for election)
     context = zmq.Context()
-    socket = context.socket(zmq.PUB)
-    port = str(basePort + myID)
-    socket.bind("tcp://*:%s" % port)
+    publisherSocket = context.socket(zmq.PUB)
+    port = basePort + myID
+    publisherSocket.bind("tcp://*:%s" % str(port))
+    # First subscriber for subscribing I'm alive msg (topic 1)
+    aliveSocket = socket.setsockopt(zmq.SUBSCRIBE, 1)
+    for i in range(len(IParray)):
+        if i != myID:
+            aliveSocket.connect(IParray[i])
+    # Second subscriber for subscribing ok msg (topic 2)
+    okSocket = socket.setsockopt(zmq.SUBSCRIBE, 2)
+    for i in range(len(IParray)):
+        if i != myID:
+            okSocket.connect(IParray[i])
+    return True
+
+
+
+
+
 
 
