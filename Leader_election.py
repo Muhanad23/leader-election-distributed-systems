5d6b82466a7f99cad852f4205e4cf08392c37012
Muhanad <muhanad.atef23@gmail.com>
2020-03-21 03:32:21 +0200

    Make Threads

--- Leader_election.py
+++ Leader_election.py
@@ -1,7 +1,7 @@
 import zmq
 import socket
 import time
-import threading
+from threading import Thread
 
 # To know if I'm the leader or not
 amIleadr = False
@@ -32,7 +32,6 @@ def initializeMySockets(IParray,myID,basePort):
         if i != myID:
             okSocket.connect(IParray[i])
     # third subscriber for subscribing elect msg (topic 3)
-    electSocket.RCVTIMEO = 1
     electSocket = context.socket(zmq.SUB)
     electSocket.setsockopt(zmq.SUBSCRIBE, "3")
     for i in range(myID+1,len(IParray)): # Connect with ID's grater than me
@@ -49,44 +48,75 @@ def elect(publisherSocket,aliveSocket,okSocket,electSocket,myID):
     except zmq.error.Again:
         # If no client with ID grater than me then I'm the leader
         amIleadr = True
-        return
+        print("I'm now the leader , my ID = %d" %myID)
+        return True
     # Else wait until there is a new leader , if waiting doesn't matter then return instead of waiting
     while True:
         try:
             message = aliveSocket.recv_string()
             message = message.split("/")
             print("A new leader with IP: %s is selected" %message[-1])
-            return
+            return False
         except zmq.error.Again:
             continue
+
+
+def sendAliveLeader(IParray,myID,publisherSocket):
+    global amIleadr
+    t = time.time()
+    while True:
+        if amIleadr == False: # Check if another leader was found
+            break
+        publish = False
+        if time.time()-t >= aliveTime:
+            t = time.time()
+            publish = True 
+        if publish:
+            publisherSocket.send_string("1 /"+IParray[myID])
+    checkForElection(IParray,publisherSocket,aliveSocket,okSocket,electSocket,myID)
+
+
+# Check if there was a leader sleeping or busy and I took its place
+def checkThereIsAnotherLeader(aliveSocket,publisherSocket,okSocket,electSocket,myID):
+    global amIleadr
+    while True:
+        try:
+            aliveSocket.recv_string()
+            amIleadr = False
+            print("I'm not the leader now")
+            break
+        except zmq.error.Again:
+            continue
+    checkIsAlive(publisherSocket,aliveSocket,okSocket,electSocket,myID)
+
+    
+def checkIsAlive(publisherSocket,aliveSocket,okSocket,electSocket,myID):
+    while True:
+        try:
+            message = aliveSocket.recv_string()
+            message = message.split("/")
+            print("Leadr with IP: %s is alive" % message[-1])
+        except zmq.error.Again:
+            print("Leader died we need election")
+            if elect(publisherSocket,aliveSocket,okSocket,electSocket,myID): # Then I'm the leader
+                break
+    checkThereIsAnotherLeader(aliveSocket,publisherSocket,okSocket,electSocket,myID)
+            
+
+def checkForElection(IParray,publisherSocket,aliveSocket,okSocket,electSocket,myID):
+    while True:
+        electSocket.recv_string()
+        if elect(publisherSocket,aliveSocket,okSocket,electSocket,myID): # Then I'm the leader
+            break
+    sendAliveLeader(IParray,myID,publisherSocket)
 
 
 
 def main(IParray,myID,basePort):
     publisherSocket,aliveSocket,okSocket,electSocket = initializeMySockets(IParray,myID,basePort)
-    t = time.time()
-    while True:
-        publish = False
-        if time.time()-t >= aliveTime:
-            t = time.time()
-            publish = True 
-        if amIleadr and publish:
-            publisherSocket.send_string("1 /"+IParray[myID])
-        else:            
-            try:
-                message = aliveSocket.recv_string()
-                message = message.split("/")
-                print("Leadr with IP: %s is alive" % message[-1])
-            except zmq.error.Again:
-                print("Leader died we need election")
-                elect(publisherSocket,aliveSocket,okSocket,electSocket,myID)
-            try :
-                electSocket.recv_string()
-                print("A client asked for election:")
-                elect(publisherSocket,aliveSocket,okSocket,electSocket,myID)
-            except zmq.error.Again:
-                pass
-        # Here the rest of code
+    # Initially there is no leader until all clients elect
+    Thread(target=checkIsAlive(publisherSocket,aliveSocket,okSocket,electSocket,myID)).start()
+    Thread(target=checkForElection(IParray,publisherSocket,aliveSocket,okSocket,electSocket,myID)).start()
             
         
 
